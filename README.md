c631a960039b8280de56cac0f30e66649ed453cc
Kendra Ryan <yesthisiskendra@gmail.com>
2022-01-26 15:42:54 -0800

    Add DataGrid

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 git push -u origin
 npm install gh-pages - save-dev
+
+npm install @material-ui/core
+npm install @material-ui/data-grid
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import Dashboard from './Dashboard';
+// import Tabs from './Tabs';
+import ToDoList from './ToDoList';
 
 function App() {
-  return <div className='App'>COMMAND CENTER!! </div>;
+  return (
+    <div className='App'>
+      {/* <Dashboard /> */}
+      {/* <Tabs /> */}
+      <ToDoList />
+    </div>
+  );
 }
 
 export default App;
