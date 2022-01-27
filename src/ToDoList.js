c9d7401410c73d596a7ba758130673b66699378b
Kendra Ryan <yesthisiskendra@gmail.com>
2022-01-26 16:03:53 -0800

    Add actual data

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,70 +2,39 @@ import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 
 const columns = [
-  // { field: 'id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
+    field: 'todo',
+    headerName: 'To Do',
+    width: 300,
     editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
-      }`,
   },
 ];
 
-// const rows = [
-//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ];
-
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, todo: 'E: GIVE DAN VIT.D + PRO.B' },
+  { id: 2, todo: 'E: GIVE PIPPIN MEDS' },
+  { id: 3, todo: 'E: FEED SOCKBOI' },
+  { id: 4, todo: 'K: TAKE MEDS' },
+  { id: 5, todo: 'K: TAKE VITAMINS' },
+  { id: 6, todo: 'A: WALK WITH DC' },
+  { id: 7, todo: 'E: FEED MAIN BABIES DINNER' },
+  { id: 8, todo: 'E: FEED SOCKBOI DINNER' },
+  { id: 9, todo: 'C: CLEAN LITTER' },
+  { id: 10, todo: 'K: DRINK TEA | MIRA + PED' },
+  { id: 11, todo: 'E: PREP DAN BFAST' },
+  { id: 12, todo: 'E: RUN DISHWASHER' },
+  { id: 13, todo: 'B: GRATITUDE JOURNAL' },
+  { id: 14, todo: 'B: LAUNDRY' },
+  { id: 15, todo: 'B: STERILIZE BOTTLES' },
 ];
 
 export default function DataTable() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 1000, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={10}
+        pageSize={15}
         checkboxSelection
         disableSelectionOnClick
       />
